48997a8da56345d101c97ade199e946831ee73df
posan <duo0405@gmail.com>
2016-08-30 11:50:37 +0800

    + add ISceneState

---
+++ StatePattern/Assets/ISceneState.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISceneState {
+
+    private string m_StateName = "ISceneState";
+    public string StateName
+    {
+        get
+        {
+            return m_StateName;
+        }
+        set
+        {
+            m_StateName = value;
+        }
+    }
+    protected SceneStateContoller m_Controller = null;
+
+    public ISceneState(SceneStateContoller Controller)
+    {
+        m_Controller = Controller;
+    }
+
+    public virtual void StateBegin()
+    {
+
+    }
+    public virtual void SteatEnd()
+    {
+
+    }
+    public virtual void StateUpdate()
+    {
+
+    }
+    public override string ToString()
+    {
+        return string.Format("[I_SceneState: StateName = {0}]", StateName);
+    }
+
+}
