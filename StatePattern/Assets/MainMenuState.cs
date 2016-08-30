b6c2216d31ac74922c94acde7bd57e89286eef74
posan <duo0405@gmail.com>
2016-08-30 13:59:59 +0800

    add MainMenuState

---
+++ StatePattern/Assets/MainMenuState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuState : ISceneState {
+
+	public  MainMenuState(SceneStateContoller Controller):base(Controller)
+    {
+
+    }
+
+    public override void StateBegin()
+    {
+        
+    }
+
+    public override void StateUpdate()
+    {
+       
+    }
+    public override void StateEnd()
+    {
+
+    }
+}
